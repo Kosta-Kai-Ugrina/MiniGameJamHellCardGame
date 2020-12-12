a5fe8cf5d3f654376ee2300eecb7eea6e6384b18
Kosta Kai Ugrina <kosta-kai.ugrina@epitech.eu>
2020-12-12 18:36:30 +0100

    added app folder and moved assets folder to it

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Juda muda guda ruda</Text>
-      <StatusBar style="dark" />
-    </View>
+    <ImageBackground
+      source={require("./app/assets/fireBackground.gif")}
+      style={styles.background}
+    >
+      <SafeAreaView style={styles.container}>
+        <Text style={{ color: "white" }}>Juda muda guda ruda</Text>
+        <StatusBar style="dark" />
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
